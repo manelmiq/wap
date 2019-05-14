d878bd00a9e7f8dd683b75b63886cbf299762c16
emmanuell <emmanuellnogmiq@hotmail.com>
2019-05-14 17:33:52 -0500

    text decorator final version

--- decorator/decoratemytext.js
+++ decorator/decoratemytext.js
@@ -1,10 +1,7 @@
-
 function biggerDecoration() {
-    var size = (document.getElementById("txtArea").style.fontSize);
+    let size = (document.getElementById("txtArea").style.fontSize);
     size = parseInt(size);
-    console.log(size);
     size += 2;
-    console.log(size);
     document.getElementById("txtArea").style.fontSize = size + "pt";
 }
 
@@ -13,7 +10,7 @@ function increaseBytime() {
 }
 
 
-function textTobold() {
+function textToBold() {
     if (document.getElementById('Bling').checked) {
         document.getElementById('txtArea').style.fontWeight = 'bold';
         document.getElementById('txtArea').style.color = 'green';
@@ -24,3 +21,67 @@ function textTobold() {
         document.getElementById('txtArea').style.textDecoration = 'none';
     }
 }
+
+
+function txtConverter() {
+    let txtArea = document.getElementById('txtArea');
+    let contentText = txtArea.textContent;
+    let textConverted = contentText.split(" ").map(malkovitchWordConverter);
+    contentText = "";
+    textConverted.forEach(function (value, index, array) {
+        contentText += (value) ;
+        console.log(index);
+        // contentText += (index > 0) ? " " : "" ;
+    });
+    txtArea.textContent = contentText;
+}
+
+
+
+function malkovitchTxtConverter() {
+    let txtArea = document.getElementById('txtArea');
+    let contentText = txtArea.textContent;
+    let textConverted = contentText.split(" ").map(malkovitchWordConverter);
+    contentText = "";
+    textConverted.forEach(function (value, index) {
+        contentText += (index > 0) ? " " : "" ;
+        contentText += (value) ;
+        console.log(index);
+        contentText += (index > 0) ? " " : "" ;
+    });
+    txtArea.textContent = contentText;
+}
+
+function malkovitchWordConverter(word) {
+    if (word.length >= 5) {
+        return "Malkovitch";
+    }
+    return word;
+}
+
+function pigLatinEvaluation(word) {
+    if(word === undefined || word === '' || word === ""){
+        return word;
+    }
+    let isVowel = ('aeiou'.indexOf( word[0].toLowerCase() ) !== -1 ) ;
+    console.log(word + " " + isVowel);
+    if(isVowel){
+        return word += "ay";
+    }
+    return word.substr(1, word.length - 1) + word[0] + "ay";
+}
+
+
+function pigLatinTxtConverter() {
+    let txtArea = document.getElementById('txtArea');
+    let contentText = txtArea.textContent;
+    let textConverted = contentText.split(" ").map(pigLatinEvaluation);
+    contentText = "";
+    textConverted.forEach(function (value, index) {
+        contentText += (index > 0) ? " " : "" ;
+        contentText += (value) ;
+    });
+    txtArea.textContent = contentText;
+}
+
+
