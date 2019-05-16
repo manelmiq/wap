6d0f89670652ab22829a8381bd4d4e14f3416323
emmanuell <emmanuellnogmiq@hotmail.com>
2019-05-16 16:40:36 -0500

    finish animation

--- animation/ascii.js
+++ animation/ascii.js
@@ -1,19 +1,22 @@
+
+
 function startAnimation() {
     let animation = document.getElementById("animation").value;
     document.getElementById("txtArea").textContent = ANIMATIONS[animation];
     document.getElementById("animation").disabled = true;
     document.getElementById("startButton").disabled = true;
     document.getElementById("stopButton").disabled = false;
-     setTimeout(function () {
+    StartTimer = setTimeout(function () {
         playAnimation(animation, 0)
     }, 250);
 }
 
 
 function stopAnimation(){
-    document.getElementById("animation").disabled = true;
-    document.getElementById("stopButton").disabled = true;
+    document.getElementById("animation").disabled = false;
     document.getElementById("startButton").disabled = false;
+    document.getElementById("stopButton").disabled = true;
+    clearTimeout(StartTimer);
 }
 
 let playAnimation = function (animation, cont) {
@@ -21,8 +24,8 @@ let playAnimation = function (animation, cont) {
     document.getElementById("txtArea").textContent = animationFrames[cont];
     let counter = (document.getElementById("turbo").checked) ? 50 : 250;
     cont = (cont + 1) %  animationFrames.length;
-    
-    setTimeout(function () {
+
+    StartTimer = setTimeout(function () {
         playAnimation(animation, cont)
     }, counter);
 };
